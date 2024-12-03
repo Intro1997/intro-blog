6f7d393d73e090a65d6dd629ca42b35b65eb701e
Intro1997 <29827323+Intro1997@users.noreply.github.com>
2024-12-03 20:57:54 +0800

    feat: update pages: 1. update About page 2. update main page header 3. update project page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'JsAudio-Android',
+    description: `Implement Web Audio by NAPI in Android. 
+    This project aims to run Web Audio API in Android with javascript code. 
+    Most of the available APIs have passed wpt test cases.`,
+    imgSrc: '/static/images/JsAudio-Android.png',
+    href: 'https://github.com/Intro1997/JsAudio-Android',
   },
 ]
 
